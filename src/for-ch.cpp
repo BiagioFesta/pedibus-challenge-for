9a8ee7865259d7be05d083602e1a82f44d3b0d5f
Biagio Festa <biagio.festa@gmail.com>
2017-01-29 16:28:04 +0100

    Added tclap library for cmd parsing

--- src/for-ch.cpp
+++ src/for-ch.cpp
@@ -1,15 +1,78 @@
 // Copyright 2017 <Biagio Festa>
+#include <tclap/CmdLine.h>
 #include <iostream>
 #include <memory>
+#include <map>
+#include <utility>
+#include <string>
 #include "ProblemDatas.hpp"
 #include "GASolver.hpp"
 
+#define _CMD_HEADER                                             \
+  "Foundation Operational Research Challenge\n"                 \
+  "   Developed by Biagio Festa and Alessandro Erba <2017>"
+
+#define _PROGRAM_VERSION "0.0.1"
+
+class FORCH_Program {
+ public:
+  FORCH_Program();
+  void run(int argc, char** argv);
+
+ private:
+  using PTR_CmdArg = std::unique_ptr<TCLAP::Arg>;
+  using StringArg = TCLAP::ValueArg<std::string>;
+  static const char CMD_HEADER[];
+  static const char VERSION[];
+
+  TCLAP::CmdLine m_cmd_parser;
+  std::map<std::string, PTR_CmdArg> m_cmd_args;
+  std::shared_ptr<for_ch::ProblemDatas> mp_problem;
+
+  void init_command_line_parse();
+};
+
+const char FORCH_Program::CMD_HEADER[] = _CMD_HEADER;
+const char FORCH_Program::VERSION[] = _PROGRAM_VERSION;
+
 int main(int argc, char *argv[]) {
-  auto ptr_problem = std::make_shared<for_ch::ProblemDatas>();
-  ptr_problem->parse_problem_dat("pedibus_100.dat");
-
-  for_ch::GASolver solver(ptr_problem);
-  solver.run(argc, argv);
-
+  FORCH_Program program;
+  try {
+    program.run(argc, argv);
+  } catch (const std::exception& err) {
+    return -1;
+  }
   return 0;
+}
+
+FORCH_Program::FORCH_Program() :
+    m_cmd_parser(CMD_HEADER, ' ', VERSION) {
+}
+
+void FORCH_Program::init_command_line_parse() {
+  PTR_CmdArg modelFile(
+      new StringArg("d",
+                    "data-file",
+                    "The dat file which describes the problem data",
+                    true,
+                    "none",
+                    "string"));
+  m_cmd_args.insert(std::make_pair("data", std::move(modelFile)));
+
+  for (const auto& arg : m_cmd_args) {
+    m_cmd_parser.add(*(arg.second));
+  }
+}
+
+void FORCH_Program::run(int argc, char** argv) {
+  init_command_line_parse();
+  m_cmd_parser.parse(argc, argv);
+
+  const std::string& dat_filename = dynamic_cast<StringArg*>(
+        m_cmd_args.at("data").get())->getValue();
+  mp_problem = std::make_shared<for_ch::ProblemDatas>();
+  mp_problem->parse_problem_dat(dat_filename);
+
+  for_ch::GASolver solver(mp_problem);
+  solver.run(argc, argv);
 }
